fd9636a30750e45447c1131778b8cd6fda538e4c
ThePeterGriffinZhenPlaz <116976348+ZhenPlaz@users.noreply.github.com>
2023-10-28 20:34:00 +0100

    Rectienvy RTM

---
+++ storeid.js
@@ -0,0 +1,97 @@
+function id1() {
+  new WinBox({
+    title: "Piskel for Envy",
+    class: "eclipsemono",
+    x: "center",
+    y: "center",
+    width: "50%",
+    height: "50%",
+    top: 0,
+    right: 0,
+    bottom: 45,
+    left: 0,
+    url: "https://www.piskelapp.com/p/create/sprite",
+  });
+}
+
+function id2() {
+  new WinBox({
+    title: "Localhost Viewer",
+    class: "eclipsemono",
+    x: "center",
+    y: "center",
+    width: "50%",
+    height: "50%",
+    top: 0,
+    right: 0,
+    bottom: 45,
+    left: 0,
+    url: "http://localhost:3000",
+  });
+}
+
+function id3() {
+  new WinBox({
+    title: "Aspen",
+    icon: "icons/aspen.png",
+    class: "eclipsemono",
+    x: "center",
+    y: "center",
+    width: "50%",
+    height: "50%",
+    top: 0,
+    right: 0,
+    bottom: 45,
+    left: 0,
+    mount: document.getElementById("TeresaMark").cloneNode(true),
+  });
+}
+
+function id4() {
+  new WinBox({
+    title: "Eaglercraft",
+    class: "eclipsemono",
+    x: "center",
+    y: "center",
+    width: "50%",
+    height: "50%",
+    top: 0,
+    right: 0,
+    bottom: 45,
+    left: 0,
+    url: "https://eaglercraft.com/",
+  });
+}
+
+function id5() {
+  new WinBox({
+    title: "Resent Client",
+    class: "eclipsemono",
+    x: "center",
+    y: "center",
+    width: "50%",
+    height: "50%",
+    top: 0,
+    right: 0,
+    bottom: 45,
+    left: 0,
+    url: "https://reslauncher.vercel.app/",
+  });
+}
+
+function id6() {
+  new WinBox({
+    title: "Calculator",
+    class: "eclipsemono",
+    x: "center",
+    y: "center",
+    width: "27%",
+    height: "70%",
+    top: 0,
+    right: 0,
+    bottom: 45,
+    left: 0,
+    url: "http://freshman-calculator.surge.sh/",
+  });
+}
+
